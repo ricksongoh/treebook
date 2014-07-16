13266eec67c4644fca0cc7e43bcdabe2f8a1da62
Rickson Goh <ricksongoh@gmail.com>
2014-07-17 00:32:36 +0800

    updated routes link to custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Treebook::Application.routes.draw do
   devise_for :users
 
-  resources :statuses
+  devise_scope :user do
 
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+    get "register", to: "devise/registrations#new", as: :register
+
+  end
+
+  resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: "statuses#index"
 
   # The priority is based upon order of creation:
